f304be168592c8a8e1464cecbae647346a8a964d
KeichiTS <kkeichi@gmail.com>
2025-09-11 16:09:11 -0300

    metodo post, variáveis do deposito

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from './generated/prisma/index.js'
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.post('/usuarios', async(req, res) => {
 
@@ -16,6 +18,33 @@ app.post('/usuarios', async(req, res) => {
   }
  })
 
+ res.status(201).json(req.body)
+
+})
+
+app.post('/rejeito', async(req, res) => {
+
+ await prisma.rejeito.create({
+  data: {
+    codigo_interno: req.body.codigo_interno,
+    categoria_id: req.body.categoria_id,
+    material_id: req.body.material_id,
+    descricao: req.body.descricao,
+    peso_kg: req.body.peso_kg,
+    dimensoes_cm: req.body.dimensoes_cm,
+    atividade_bq: req.body.atividade_bq,
+    data_medicao_atividade: req.body.data_medicao_atividade,
+    tempo_decaimento_dias: req.body.tempo_decaimento_dias,
+    nivel_referencia_bq: req.body.nivel_referencia_bq,
+    data_recebimento: req.body.data_recebimento,
+    data_liberacao: req.body.data_liberacao,
+    status_id: req.body.status_id,
+    local_armazenamento_id: req.body.local_armazenamento_id
+
+  }
+ })
+
+ console.log(req.body)
  res.status(201).json(req.body)
 
 })
@@ -37,8 +66,6 @@ app.get('/usuarios', async (req, res) => {
 
  console.log(req)
  res.status(200).json(users)
-
-
 
 })
 
